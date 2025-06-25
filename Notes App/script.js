36dfd9c8787e43f26d7f37c79944e70688968ef1
bl4kcy <mostafafarssiui@gmail.com>
2025-06-25 17:15:38 +0100

    Refactor note structure and styles in Notes App for improved usability

--- Notes App/script.js
+++ Notes App/script.js
@@ -1,37 +1,60 @@
 const createNoteBtn = document.getElementById('create-note-btn');
 const notesContainer = document.getElementById('notes-container');
 const titleMaxLenght = 30;
-console.log(notesContainer);
-
 
 createNoteBtn.addEventListener('click', event => {
 	const note = document.createElement('div');
-	const title = document.createElement('h3');
+	const title = document.createElement('input');
+	// const titleP = document.createElement()
 	const content = document.createElement('textarea');
 	const del = document.createElement('i');
 	const date = new Date();
 
 	note.className = 'note'
 	title.className = 'note-title'
+	title.name = 'title'
+	title.maxLength = 36;
+	title.value = 'Untitled | ' + Date().split('GMT')[0]
+	title.placeholder = 'Title'
+	title.oninput = e => {
+		SaveChanges();
+	}
+	title.onfocus = e => {
+		title.select();
+	}
 	content.className = 'note-content'
+	content.name = 'note'
+	content.placeholder = 'Content'
+	content.oninput = e => {
+		SaveChanges();
+	}
 	del.className = 'delete-note-btn'
-
-	title.textContent = 'untitled | ' + Date().split('GMT')[0]
-	title.setAttribute('contenteditable', 'true')
 	del.dataset.lucide = 'trash'
-	title.addEventListener('input', e => {
-		// here need to fix max length title 
-		// and prevent the newline 
-	})
 	note.addEventListener('click', e => {
 		if (e.target.classList.contains('lucide') || e.target.parentElement.classList.contains('lucide')){
 			note.remove();
+			SaveChanges();
 		}
 	})
-	
 	note.appendChild(title);
 	note.appendChild(content);
 	note.appendChild(del);
-	notesContainer.appendChild(note);
+	notesContainer.insertBefore(note, notesContainer.firstChild)
 	lucide.createIcons();
+	title.focus();
+	SaveChanges();
 })
+
+
+function SaveChanges() {
+	console.log(notesContainer.innerHTML);
+	localStorage.setItem('note-data', notesContainer.innerHTML);
+}
+
+function loadData() {
+	const data = localStorage.getItem('note-data');
+	notesContainer.innerHTML = data
+}
+
+
+// loadData();
