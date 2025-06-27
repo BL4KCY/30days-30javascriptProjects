af5c935c6816625930b47a94893a2d2eeb90f109
bl4kcy <mostafafarssiui@gmail.com>
2025-06-27 19:24:17 +0100

    Add QR Code Generator implementation with HTML, CSS, and JavaScript

---
+++ QR Code Generator/script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('.container > div > input')
+const color = document.getElementById('color')
+const qrCodeImage = document.querySelector('.container > img');
+const QRCodeApi = 'https://api.qrserver.com/v1/create-qr-code/?size=900x900&format=svg&data='
+
+async function generateQrCode() {
+	console.log(QRCodeApi + encodeURIComponent(input.value) + '&color=' + color.value.replace('#', ''))
+	
+	qrCodeImage.src = QRCodeApi + encodeURIComponent(input.value) + '&color=' + color.value.replace('#', '')
+}
