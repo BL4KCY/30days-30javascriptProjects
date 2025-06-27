7f50cd3735a287615aac1a486e9b03b785d89253
bl4kcy <mostafafarssiui@gmail.com>
2025-06-27 20:05:29 +0100

    Add error handling and shake animation for empty input in QR Code Generator

--- QR Code Generator/script.js
+++ QR Code Generator/script.js
@@ -4,8 +4,14 @@ const qrCodeImage = document.querySelector('#qr-code > img');
 const QRCodeApi = 'https://api.qrserver.com/v1/create-qr-code/?size=900x900&format=svg&data='
 
 async function generateQrCode() {
-	console.log(QRCodeApi + encodeURIComponent(input.value) + '&color=' + color.value.replace('#', ''))
-	
-	qrCodeImage.src = QRCodeApi + encodeURIComponent(input.value) + '&color=' + color.value.replace('#', '')
-	qrCodeImage.parentElement.classList.add('show-img');
+	if (input.value == '') {
+		input.parentElement.classList.add('error');
+		setTimeout(()=>{
+			input.parentElement.classList.remove('error')
+		}, 500)
+	} else {
+		console.log(QRCodeApi + encodeURIComponent(input.value) + '&color=' + color.value.replace('#', ''))
+		qrCodeImage.src = QRCodeApi + encodeURIComponent(input.value) + '&color=' + color.value.replace('#', '')
+		qrCodeImage.parentElement.classList.add('show-img');
+	}
 }
