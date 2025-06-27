62da2425e0df172010fd7e9f530b22e70535f9c3
bl4kcy <mostafafarssiui@gmail.com>
2025-06-27 16:15:50 +0100

    Implement Quote Generator functionality with API integration and dynamic quote display

---
+++ Quote Generator/script.js
@@ -0,0 +1,29 @@
+const QuoteApiURL = 'https://api.api-ninjas.com/v1/quotes'
+const QuoteApiKey = 'sucP+CHclvdJ01ySUbg6gA==6pbVPDFsKt9aGcFz'
+const blockQuote = document.querySelector('.quote-card > blockquote');
+const author = document.querySelector('.quote-card > span');
+
+
+async function randomQuote(params) {
+	const response = await fetch(QuoteApiURL, {
+		method: 'GET',
+		headers: {
+			'X-Api-Key': QuoteApiKey
+		}
+	})
+	if (response.status == 404) {
+		alert("Sorry Api problem please try later!");
+	} else {
+		const data = await response.json()
+		blockQuote.textContent = data[0].quote;
+		author.textContent = data[0].author;
+	}	
+}
+
+function shareX() {
+	const content = encodeURIComponent("\"" + blockQuote.textContent + "\"" + '\n\n--' + author.textContent + ' 🖊️');
+	const url = 'https://x.com/intent/post?text=' + content
+	window.open(url, '_blank');
+}
+
+randomQuote();
