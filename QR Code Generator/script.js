24c36baf7fa3b1a32a4b04dc1860bf148fe5aeaf
bl4kcy <mostafafarssiui@gmail.com>
2025-06-27 19:43:26 +0100

    Refactor QR Code Generator layout and styles for improved structure and visibility

--- QR Code Generator/script.js
+++ QR Code Generator/script.js
@@ -1,10 +1,11 @@
 const input = document.querySelector('.container > div > input')
 const color = document.getElementById('color')
-const qrCodeImage = document.querySelector('.container > img');
+const qrCodeImage = document.querySelector('#qr-code > img');
 const QRCodeApi = 'https://api.qrserver.com/v1/create-qr-code/?size=900x900&format=svg&data='
 
 async function generateQrCode() {
 	console.log(QRCodeApi + encodeURIComponent(input.value) + '&color=' + color.value.replace('#', ''))
 	
 	qrCodeImage.src = QRCodeApi + encodeURIComponent(input.value) + '&color=' + color.value.replace('#', '')
+	qrCodeImage.parentElement.classList.add('show-img');
 }
